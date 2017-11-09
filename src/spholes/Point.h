745c74019c6345e7f708b0235eb56a0bb07035c1
Kevin Zúñiga <kevin.zun@gmail.com>
2017-11-09 17:15:37 -0500

    Fix point getters declarations to const

--- src/spholes/Point.h
+++ src/spholes/Point.h
@@ -10,8 +10,8 @@ class Point : public std::complex<real>
 {
 public:
     Point(spholes::real x, spholes::real y) : std::complex<spholes::real> (x, y) {}
-    inline spholes::real x() { return this->real(); }
-    inline spholes::real y() { return this->imag(); }
+    inline spholes::real x() const { return this->real(); }
+    inline spholes::real y() const { return this->imag(); }
 };
 
 }
