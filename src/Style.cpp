8bf61d650713b118dff771f2f8e39779aef6e8ea
Kevin Zúñiga <kevin.zun@gmail.com>
2018-01-29 12:07:40 -0500

    Update pen style for obstacles and shortest path

--- src/Style.cpp
+++ src/Style.cpp
@@ -3,11 +3,11 @@
 const QColor Style::startPointColor = QColor(224, 43, 42);
 const QColor Style::destinationPointColor = QColor(255, 133, 16);
 
-const QPen Style::obstaclePen = QPen(QColor(63, 111, 160));
+const QPen Style::obstaclePen = QPen(QBrush(QColor(63, 111, 160)), 2);
 const QBrush Style::obstacleBrush = QBrush(QColor(79, 139, 201, 128));
 
 const QColor Style::newPolygonStartPointColor = QColor(63, 111, 160);
 const QPen Style::newPolygonPen = QPen(QColor(63, 111, 160));
 
-const QPen Style::shortestPathPen = QPen(QBrush(QColor(41, 42, 45)), 1.2, Qt::SolidLine);
+const QPen Style::shortestPathPen = QPen(QBrush(QColor(41, 42, 45)), 2, Qt::SolidLine);
 const QPen Style::visibilityGraphPen = QPen(QBrush(QColor(89, 92, 102)), 0.5, Qt::DashLine);
